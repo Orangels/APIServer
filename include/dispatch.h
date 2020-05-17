34a83d8b9e17bf39c2e7f9ae50624a01f467a115
Orangels <orangels0313@163.com>
2020-05-17 19:32:01 +0800

    del rtmp, gearman, TODO TensorRT , db config and RPCServer

--- include/dispatch.h
+++ include/dispatch.h
@@ -8,18 +8,15 @@
 
 //ls add
 #include <opencv2/core.hpp>
-#include <libgearman/gearman.h>
 #include <mutex>
 #include <queue>
 #include <thread>
 #include <vector>
 #include <condition_variable>
-#include "rtmpHandler.h"
 #include "config.h"
 #include "utils/track.h"
 #include "utils/match_id.h"
 #include <Python.h>
-#include "EnginePy.hpp"
 
 using namespace std;
 class Dispatch
@@ -37,8 +34,7 @@ class Dispatch
         int mQueueLen = 5;
 
 //        front cam params
-//        queue<cv::Mat> mQueue_front;
-        queue<vector<cv::Mat>> mQueue_front;
+        queue<cv::Mat> mQueue_front;
         queue<cv::Mat> mQueue_rtmp_front;
 
         condition_variable con_front_not_full;
@@ -51,13 +47,12 @@ class Dispatch
 
         mutex rtmpMutex_front;
 
-        rtmpHandler ls_handler_front;
         cv::Mat rtmp_front_img;
 
         bool camera_front;
 
     //        middle cam params
-        queue<vector<cv::Mat>> mQueue_mid;
+        queue<cv::Mat> mQueue_mid;
         queue<cv::Mat> mQueue_rtmp_mid;
 
         condition_variable con_mid_not_full;
@@ -70,20 +65,13 @@ class Dispatch
 
         mutex rtmpMutex_mid;
 
-        rtmpHandler ls_handler_mid;
         cv::Mat rtmp_mid_img;
 
         bool camera_mid;
 
 //        rpc params
-        gearman_return_t gearRet;
-        gearman_return_t gearWRet;
-        gearman_client_st* gearClient;
-        gearman_worker_st* gearWorker;
 
 
-//        PyObject* pIns;
-//        PyObject* pIns1;
         Match_ID* matcher;
 
 private:
@@ -98,11 +86,11 @@ private:
         void ConsumeRTMPImage(int mode);
         void RPCServer();
 
-        Engine_api * pyEngineAPI;
-
-        Engine_api * pyEngineAPI_0;
-        Engine_api * pyEngineAPI_1;
-        Engine_api * pyEngineAPI_2;
+//        Engine_api * pyEngineAPI;
+//
+//        Engine_api * pyEngineAPI_0;
+//        Engine_api * pyEngineAPI_1;
+//        Engine_api * pyEngineAPI_2;
 
 
 };
